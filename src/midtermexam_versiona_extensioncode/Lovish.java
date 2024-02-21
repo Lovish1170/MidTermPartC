a0dac67335ceecd3c2ce7c78b17217fa8879f731
Pratap Singh <Pratap Singh@10.48.17.251>
2024-02-21 10:35:47 -0500

    my code comments

---
+++ src/midtermexam_versiona_extensioncode/Lovish.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author Pratap Singh
+ */
+public class Lovish {
+    public static void main(String[] args) {
+    System.out.println("My name is Lovish Lovish");
+    System.out.println("My Student ID is 991716778");}
+}
